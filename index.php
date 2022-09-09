b41337e9a3f1fb5c363951bb8120347d4e4e6a02
oliwierbogdanski <92371275+oliwierbogdanski@users.noreply.github.com>
2022-09-09 07:17:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+
+</head>
+<body>
+    <h1>
+         pierwszy raz w php 
+    </h1>
+    <?php 
+    echo 'Chciałem być szefem
+    I nigdy więcej zapierdalać
+    Machać moim wrogom świeżym cashem
+    I dziś siedzę se tu jak ten prezes
+    Uhhhhh
+    Z złotym rolexem
+    Mama mówiła synku uważaj bo
+    To niebezpieczne
+    Zbyt wielu karaluchów jest na świecie
+    Dlatego dziś na deser będzie Desert';
+     ?>
+</body>
+</html>
